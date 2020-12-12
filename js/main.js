7aea22d554d7e98338cd5c3f6c157100003be308
Yura <yuralvov754755@gmail.com>
2020-12-12 23:14:21 +0200

    lecture10' git commit -mlecture10
    git commit -mlecture10
    git commit -m"lecture10"

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const handlePlay = (keyCode) => {
+    const song = document.querySelector(`.song-${keyCode}`);
+    const button = document.querySelector(`.button-${keyCode}`);
+    if (song && button) {
+        song.play();
+        button.classList.toggle('active');
+        // setTimeout(() => button.classList.toggle('active'), 1e3);
+    }
+
+};
+document.addEventListener('keydown', (e) => handlePlay(e.code));
+document.addEventListener('mouseover', (e) => {
+    const originalTarget = e.target.closest('div[data-keycode]');
+    if (originalTarget) {
+        const {
+            keycode
+        } = originalTarget.dataset;
+        handlePlay(keycode);
+    }
+
+});
